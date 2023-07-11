1dbe76691d4fa6e444862b167e52bf947aa245f6
Julia Martsenko <jm@letmespeak.org>
2023-07-11 18:37:41 +0400

    fixed removing of badges when app is opened

--- MyPeriodsTracker/MyPeriodsTracker/Main/MyPeriodsTrackerApp.swift
+++ MyPeriodsTracker/MyPeriodsTracker/Main/MyPeriodsTrackerApp.swift
@@ -11,10 +11,16 @@ import BackgroundTasks
 @main
 struct MyPeriodsTrackerApp: App {
 	@ObservedObject var mainPeriodViewModel = MainPeriodViewModel()
+	@Environment(\.scenePhase) var scenePhase
 
     var body: some Scene {
         WindowGroup {
 			MainPeriodsView(viewModel: mainPeriodViewModel)
+				.onChange(of: scenePhase) { newPhase in
+					if newPhase == .active {
+						UIApplication.shared.applicationIconBadgeNumber = 0
+					}
+				}
         }
     }
 }
